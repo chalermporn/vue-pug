8dd61283463e252b8674bce0e94b3dbb15be4c24
BIRD-MPC <codemunha@gmail.com>
2020-06-29 23:16:09 +0700

    Update

---
+++ precache-manifest.6d2f8c2b4f934251dc49d4502392eefa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c15e28a41a31ca55a5",
+    "url": "css/app.ddff14f9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8a51f3cace147bbf167db34eeddf4475",
+    "url": "index.html"
+  },
+  {
+    "revision": "1f5362170688370c40bb",
+    "url": "js/about.ebaee409.js"
+  },
+  {
+    "revision": "a2c15e28a41a31ca55a5",
+    "url": "js/app.d78e2dce.js"
+  },
+  {
+    "revision": "f9a7078613e7dd666888",
+    "url": "js/chunk-vendors.0f4403ed.js"
+  },
+  {
+    "revision": "3c02b95c482f95aa6aa67728d89a1432",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
