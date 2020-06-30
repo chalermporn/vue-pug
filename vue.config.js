3101346b274c9f180e474475337ad7cd1eafdaa5
BIRD-MPC <codemunha@gmail.com>
2020-06-30 23:09:34 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
+const merge = require('lodash')
+
 module.exports = {
   parallel: false,
-  publicPath: ''
+  publicPath: '',
+  // chainWebpack: config => {
+  //   config.module
+  //     .rule('postcss')
+  //     .test(/\.css$/)
+  //     .use('postcss-loader')
+  //     .tap(options => {
+  //       options.importLoaders = 1
 
+  //       return options
+  //     })
+  // }
+  chainWebpack: config => {
+    config.module
+      .rule('pcss')
+      .use('postcss-loader')
+      .tap(options =>
+        merge(options, {
+          sourceMap: false
+        })
+      )
+  }
 }
